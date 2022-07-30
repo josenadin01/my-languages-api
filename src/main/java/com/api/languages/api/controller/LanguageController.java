8250c778e7847f19c076194a71d2ed7b436f8840
José Nadin <josenadin01@gmail.com>
2022-07-30 10:41:25 -0300

    Feat: New get by id

--- src/main/java/com/api/languages/api/controller/LanguageController.java
+++ src/main/java/com/api/languages/api/controller/LanguageController.java
@@ -9,56 +9,60 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping("/languages")
 public class LanguageController {
 
     @Autowired
     private LanguageRepository languageRepo;
 
-    @GetMapping("/languages")
+    @GetMapping
     public List<Language> getLanguages() {
         List<Language> languages = languageRepo.findAll();
         return languages;
     }
+    
+    @GetMapping("/{id}")
+    public Language getLanguageById(@PathVariable String id) {
+        Language language = languageRepo.findById(id).orElseThrow();
+        return language;
+    }
 
-    @GetMapping("/languages/ranking")
+    @GetMapping("/ranking")
     public List<Language> getRankedList() {
         List<Language> rankedList = languageRepo.findAll(Sort.by("ranking"));
         return rankedList;
     }
 
-    @PostMapping("/languages")
-    public Language newLanguage(@RequestBody Language language) {
-        Language savedLanguage = languageRepo.save(language);
-        return savedLanguage;
-    }
+    // Comentado para não ter que fazer backup dos dados em caso de alteração.
+    // @PostMapping("/languages")
+    // public Language newLanguage(@RequestBody Language language) {
+    //     Language savedLanguage = languageRepo.save(language);
+    //     return savedLanguage;
+    // }
 
-    @PutMapping("/languages/{id}")
-    public ResponseEntity<Language> updateLanguage(@PathVariable String id, @RequestBody Language language) {
-        Language newLanguage = languageRepo.findById(id).orElseThrow();
-        newLanguage.setTitle(language.getTitle());
-        newLanguage.setImage(language.getImage());
-        newLanguage.setRanking(language.getRanking());
-        languageRepo.save(newLanguage);
-        return ResponseEntity.ok(newLanguage);
-    }
+    // @PutMapping("/languages/{id}")
+    // public ResponseEntity<Language> updateLanguage(@PathVariable String id, @RequestBody Language language) {
+    //     Language newLanguage = languageRepo.findById(id).orElseThrow();
+    //     newLanguage.setTitle(language.getTitle());
+    //     newLanguage.setImage(language.getImage());
+    //     newLanguage.setRanking(language.getRanking());
+    //     languageRepo.save(newLanguage);
+    //     return ResponseEntity.ok(newLanguage);
+    // }
 
-    @DeleteMapping("languages/{id}")
-    public String deleteLanguage(@PathVariable String id) {
-        try {
-            languageRepo.deleteById(id);
-            return "Apagado com sucesso";
-        } catch (Exception e) {
-            return "Id não encontrado";
-        }
+    // @DeleteMapping("languages/{id}")
+    // public String deleteLanguage(@PathVariable String id) {
+    //     try {
+    //         languageRepo.deleteById(id);
+    //         return "Apagado com sucesso";
+    //     } catch (Exception e) {
+    //         return "Id não encontrado";
+    //     }
 
-    }
+    // }
 }
